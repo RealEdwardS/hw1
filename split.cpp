3e62dd146417950cdfcb447338e294f917593594
Edward Shao <shaoe@usc.edu>
2024-01-21 23:04:04 -0800

    all done with part 1

--- split.cpp
+++ split.cpp
@@ -12,12 +12,88 @@ the function below should be the only one in this file.
 
 #include "split.h"
 
+
+// WRITE YOUR CODE HERE
 /* Add a prototype for a helper function here if you need */
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
-// WRITE YOUR CODE HERE
+
+  // If original list is empty
+  if (in == nullptr){
+    return;
+  }
+
+  // If value is even
+  if ((in->value % 2) == 0){
+    // If evens list is empty / first pointer is nullptr
+    if (evens == nullptr){
+      // Assign evens node to current node from "in" list
+      evens = in; 
+
+      // Assign current "in" pointer to the next value in the "in" list
+      in = in->next; 
+
+      // Assign the next value of the current node to nullptr
+      evens->next = nullptr;
+
+      // Call split
+      split(in, odds, evens); 
+    }
+
+
+    // If evens list is not empty / first pointer is not nullptr 
+    else if (evens != nullptr){
+      // Assign next value of the current node of evens to the current "in" node 
+      evens->next = in;
+
+      // Assign current "in" pointer ot the next value in the "in" list 
+      in = in->next;
+
+      // Assign the next value of the current node to nullptr
+      evens->next->next = nullptr;
+
+      // Call split on the next node - 
+      // this makes sure we aren't repeatedly calling on the first node over and over again
+      split(in, odds, evens->next); 
+    }
+  }
+
+  // If the value is odd 
+  else if ((in->value % 2) != 0){
+    // If odds list is empty / first pointer is nullptr
+    if (odds == nullptr){
+      // Assign odds node to current node from "in" list
+      odds = in;
+
+      // Assign current "in" pointer to the next value in the "in" list
+      in = in->next;
+
+      // Assign the next value of the current node to nullptr
+      odds->next = nullptr;
+
+      // Call split
+      split(in, odds, evens); 
+    }
+
+    // If odds list is not empty / first pointer is not nullptr 
+    else if (odds != nullptr){
+      // Assign next value of the current node of odds to the current "in" node 
+      odds->next = in;
+
+      // Assign current "in" pointer ot the next value in the "in" list 
+      in = in->next;
+
+      // Assign the next value of the current node to nullptr
+      odds->next->next = nullptr;
+
+      // Call split on the next node - 
+      // this makes sure we aren't repeatedly calling on the first node over and over again
+      split(in, odds->next, evens); 
+    }
+  }
+  
 }
 
 /* If you needed a helper function, write it here */
--- test_split.cpp
+++ test_split.cpp
@@ -13,5 +13,47 @@ g++ split.cpp test_split.cpp -o test_split
 
 int main(int argc, char* argv[])
 {
+    Node* in = nullptr; 
+
+    Node* node1 = new Node(2, nullptr); 
+    Node* node2 = new Node(2, nullptr);
+    Node* node3 = new Node(3, nullptr); 
+    Node* node4 = new Node(2, nullptr); 
+    Node* node5 = new Node(2, nullptr); 
+    Node* node6 = new Node(2, nullptr); 
+    Node* node7 = new Node(2, nullptr); 
+    Node* node8 = new Node(2, nullptr); 
+    Node* node9 = new Node(2, nullptr); 
+    Node* node10 = new Node(10, nullptr); 
+
+    in = node1; 
+    node1->next = node2; 
+    node2->next = node3; 
+    node3->next = node4; 
+    node4->next = node5; 
+    node5->next = node6; 
+    node6->next = node7; 
+    node7->next = node8; 
+    node8->next = node9; 
+    node9->next = node10;
+
+    Node* evens = nullptr; 
+    Node* odds = nullptr; 
+
+    split(in, odds, evens);
+
+    delete node1;
+    delete node2;
+    delete node3;
+    delete node4;
+    delete node5;
+    delete node6;
+    delete node7;
+    delete node8;
+    delete node9;
+    delete node10; 
+
+    return 0;
+
 
 }
